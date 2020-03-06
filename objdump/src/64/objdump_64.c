f3a1d4ba1b19d565c536db84707bef6494588463
Guillaume Bonnet <guillaume.bonnet@epitech.eu>
2020-03-06 14:38:19 +0100

    Clean code

--- objdump/src/64/objdump_64.c
+++ objdump/src/64/objdump_64.c
@@ -10,8 +10,24 @@
 #include <string.h>
 
 #include "../common/def.h"
+#include "../common/struct.h"
 #include "elf_64.h"
 #include "section_64.h"
+
+static void objdump_64_section_print(
+    const Elf64_Ehdr *elf_hdr, const Elf64_Shdr *sec_hdr)
+{
+    if (sec_hdr->sh_name == 0)
+        return;
+
+    char *sec_str = section_64_get_name(sec_hdr, elf_hdr);
+
+    for (int j = 0; ELF_SEC_NAME_SKIP[j]; ++j)
+        if (strcmp(sec_str, ELF_SEC_NAME_SKIP[j]) == 0)
+            return;
+
+    section_64_print(sec_hdr, elf_hdr);
+}
 
 int objdump_64(const char *path, const Elf64_Ehdr *elf_hdr)
 {
@@ -22,16 +38,7 @@ int objdump_64(const char *path, const Elf64_Ehdr *elf_hdr)
     for (int i = 0; i < elf_hdr->e_shnum; ++i) {
         Elf64_Shdr *sec_hdr = &sec_hdr_table[i];
 
-        if (sec_hdr->sh_name == 0) continue;
-
-        char *sec_str = section_64_get_name(sec_hdr, elf_hdr);
-
-        if (strcmp(sec_str, ".bss") == 0) continue;
-        if (strcmp(sec_str, ".shstrtab") == 0) continue;
-        if (strcmp(sec_str, ".strtab") == 0) continue;
-        if (strcmp(sec_str, ".symtab") == 0) continue;
-
-        section_64_print(sec_hdr, elf_hdr);
+        objdump_64_section_print(elf_hdr, sec_hdr);
     }
 
     return (SUCCESS);
--- objdump/src/common/struct.h
+++ objdump/src/common/struct.h
@@ -15,12 +15,31 @@ typedef struct elf_machine_arch_s {
     unsigned long e_machine;
 } elf_machine_arch_t;
 
+typedef struct elf_type_flag_s {
+    unsigned long flag;
+    unsigned short e_type;
+} elf_type_flag_t;
+
+typedef struct elf_sec_name_flag_s {
+    unsigned long flag;
+    char *name;
+} elf_sec_name_flag_t;
+
+typedef struct elf_sec_type_flag_s {
+    unsigned long flag;
+    unsigned int sh_type;
+} elf_sec_type_flag_t;
+
 typedef struct elf_bfd_flag_s {
     char *str;
     unsigned long flag;
 } elf_bfd_flag_t;
 
+extern const char *ELF_SEC_NAME_SKIP[];
 extern const elf_machine_arch_t ELF_MACHINE_ARCH[];
+extern const elf_type_flag_t ELF_TYPE_FLAGS[];
+extern const elf_sec_name_flag_t ELF_SEC_NAME_FLAGS[];
+extern const elf_sec_type_flag_t ELF_SEC_TYPE_FLAGS[];
 extern const elf_bfd_flag_t ELF_BFD_FLAGS[];
 
 #endif  // OBJDUMP_STRUCT_H
